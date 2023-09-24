c3956eda221dbaa0e641cb49a8d2a58e2ef375bf
Fred Denis <fredjamo68@gmail.com>
2023-09-25 00:44:51 +0300

    Lazy  loading

--- src/app/components/CityList.js
+++ src/app/components/CityList.js
@@ -1,17 +1,21 @@
-// components/CityList.js
 import React from 'react';
-import LazyLoad from 'react-lazyload';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/opacity.css'; // You can add more CSS effects if needed
 import CityCard from './CityCard';
 
 const CityList = ({ cities }) => {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
             { cities.map((city, index) => (
-                <LazyLoad key={ index } height={ 200 } offset={ 100 }>
-                    <div className="animate__animated animate__fadeIn">
-                        <CityCard city={ city } />
-                    </div>
-                </LazyLoad>
+                <div key={ index } className="animate__animated animate__fadeIn">
+                    <CityCard city={ city } />
+                    <LazyLoadImage
+                        alt={ city.City }
+                        src={ city.imageURL } // Replace with the actual image URL property from your data
+                        height={ 200 }
+                        effect="opacity" // You can customize the loading effect
+                    />
+                </div>
             )) }
         </div>
     );
